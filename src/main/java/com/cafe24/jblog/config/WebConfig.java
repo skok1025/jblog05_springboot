7787a3f75508705a506a3d16b123c448f4616e5d
kim <skok1025@naver.com>
2019-06-05 14:28:27 +0900

    config

--- src/main/java/com/cafe24/jblog/config/WebConfig.java
+++ src/main/java/com/cafe24/jblog/config/WebConfig.java
@@ -5,13 +5,16 @@ import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -86,10 +89,8 @@ public class WebConfig implements WebMvcConfigurer{
 
 	}
 	
-
-	
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/images/**").addResourceLocations("file:/mysite-uploads/");
+		registry.addResourceHandler("/logos/**").addResourceLocations("file:/jblog-uploads/");
 	}
 	
 	
